bb5c9ccb20e6ca60725f36ca1a95c59bd6b997ea
Harnoor Kaur <harnoor6ninthbhvm@gmail.com>
2026-03-01 22:30:23 +0530

    Problem Solving qs

---
+++ ProblemSolving.py
@@ -0,0 +1,33 @@
+# name = "Harnoor"
+# age = 23
+# address = "123 Main Street"
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Address:", address)
+
+# x = 12
+# y = 23
+# temp = x
+# x = y
+# y = temp
+
+# print("After swapping:")
+# print("x:", x)  
+# print("y:", y) 
+
+# x = x + y
+# y = x - y
+# x = x - y
+
+# print("After swapping:")
+# print("x:", x)      
+# print("y:", y)
+
+# x = 3.9
+# print(type(x))  
+
+# x = int(x)
+# print(type(x))
+a = int(input("Enter a number: "))
+print(float(a))
