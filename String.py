b3445b532262033e864be411af8c9928ee9ff30d
Harnoor Kaur <harnoor6ninthbhvm@gmail.com>
2026-03-11 23:32:55 +0530

    String methods

---
+++ String.py
@@ -0,0 +1,15 @@
+a = "hello {}"
+n = "Sahil"
+print(type(a))
+print(a)
+
+print(len(a))
+print(a.count('o'))
+print(a.lower())
+print(a.upper())
+print(a.index("h"))
+print(a.capitalize())
+print(a.casefold()) 
+print(a.find("e"))
+print(a.format(n))
+print(a.center(20, "*"))
