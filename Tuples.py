dca8715b13476ae3f87fc9bee0d566cc71c428d6
Harnoor Kaur <harnoor6ninthbhvm@gmail.com>
2026-03-23 14:32:53 +0530

    added Dictionary Functions

--- Tuples.py
+++ Tuples.py
@@ -4,19 +4,36 @@
 
 # b = ("Ironman",)
 
-a = ("OnePlus", "Vivo", "Realme", "Samsumg", "Nokia")
+
 # print(a[1:3])
 # print(a[2:])
 # print(a[::2])
 # print(a[::-1])
 
-for i in a:
-    print(i)
+# for i in a:
+#     print(i)
 
-for i in range(0, len(a)):
-    print(a[i])
+# for i in range(0, len(a)):
+#     print(a[i])
 
-i = 0
-while i < len(a):
-    print(a[i])
-    i += 1
+# i = 0
+# while i < len(a):
+#     print(a[i])
+#     i += 1
+
+# a = ("OnePlus", "Vivo", "Realme", "Samsumg", "Nokia")
+# print(type(a))
+
+# a = list(a)
+# print(type(a))
+# a.append("Iphone")
+# print(a)
+
+# a = tuple(a)
+# print(type(a))
+
+# # Tuple Methods
+# print(a.count("Vivo"))
+# print(a.index("Nokia"))
+
+
---
+++ dictionary.py
@@ -0,0 +1,78 @@
+# Employee_data = {"name" : "John", "age" : 24, "gender" : "male"}
+# print(Employee_data)
+# print(Employee_data["gender"])
+
+# Iteration in Dictionaries
+# Student = {"name" : "John", "class" : 6, "rollNo" : 9}
+# print(Student["class"])
+
+# # Keys
+# for x in Student:
+#     print(x)
+
+# # Values
+# for x in Student:
+#     print(Student[x])
+
+# for x in Student.values():
+#     print(x)
+
+# # Keys + Values
+# for x,y in Student.items():
+#     print(x,"=",y)
+
+# Student = {"name" : "John", "class" : 6, "rollNo" : 9}
+
+# x = Student.get("name")
+# print(x)
+
+# a = Student.items()
+# print(a)
+
+# b = Student.keys()
+# print(b)
+
+# c = Student.values()
+# print(c)
+
+# d = Student.copy()
+# print(d)
+
+# Student = {"name" : "John", "class" : 6, "rollNo" : 9}
+# x = Student.setdefault("rollno", 24)
+# print(x)
+
+# Employee = {1 : {"Name" : "Harnoor", "Age" : 24, "Gender" : "female"},
+#             2 : {"Name" : "Lisa", "Age" : 34, "Gender" : "female"},
+#             3 : {"Name" : "Sahil", "Age" : 20, "Gender" : "male"}
+#             }
+# print(Employee[2]["Age"])
+
+# a = {"a" : 12, "b" : 13, "c" : 11 }
+# a = sorted(a.values())
+# print(a)
+
+# a = {}
+# for i in range(1, 16):
+#     a[i] = i**2
+
+# print(a)
+
+# a = {"a" : 1, "b" : 4, "c" : 11 }
+# mul = 1
+# for i in a:
+#     mul *= a[i]
+# print(mul)
+
+a = {"a" : 12, "b" : 13, "c" : 11 , "d" : 78, "e" : 23}
+a = sorted(a.keys())
+print(a)
+
+
+
+
+
+
+
+
+
