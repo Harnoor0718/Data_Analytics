f87ab5801401d9228ea28568df847797a7398169
Harnoor Kaur <harnoor6ninthbhvm@gmail.com>
2026-03-24 23:53:47 +0530

    added Problem Solving Sets

--- Sets.py
+++ Sets.py
@@ -27,29 +27,56 @@ a = {"Harnoor", "Arun", "Sahil", "Ram", "Aditi"}
 # print(c.issuperset(a))
 
 
-a = {"Ironman", "Hulk", "Thor", "Captain America"}
-b = {"Superman", "Batman", "Wonder-Woman"}
-c = {"Hulk", "Thor", "SpiderMan"}
+# a = {"Ironman", "Hulk", "Thor", "Captain America"}
+# b = {"Superman", "Batman", "Wonder-Woman"}
+# c = {"Hulk", "Thor", "SpiderMan"}
 
-# Union 
-print(a.union(b))
+# # Union 
+# print(a.union(b))
 
-# Difference
+# # Difference
+# print(a.difference(b))
+
+# # Difference Update
+# a.difference_update(c)
+# print(a)
+
+# # Intersection
+# x  = a.intersection(c)
+# print(x)
+
+# a.intersection_update(c)
+# print(a)
+
+# y = a.symmetric_difference(c)
+# print(y)
+
+# To find max and min in set 
+a = {12, 13, 56, 99, 34}
+print(max(a))
+print(min(a))
+
+# To find common elements in three lists using sets
+a1 = [1, 5, 8, 9]
+a2 = [3, 4, 5]
+a3 = [3, 4, 5, 8, 9]
+print(set(a1) & set(a2) & set(a3))
+
+# To find diff bw two sets
+a = {1, 5, 8, 9}
+b = {3, 4, 5}
 print(a.difference(b))
 
-# Difference Update
-a.difference_update(c)
+# To remove an item from a set if it is present in set
+a = {1, 5, 8, 9}
+a.discard(8)
 print(a)
 
-# Intersection
-x  = a.intersection(c)
-print(x)
+# To check if a set is a subset of another set
+a = {1, 2, 3, 4, 5, 6}
+b = {2, 3, 4}
+print(a.issubset(b))
 
-a.intersection_update(c)
-print(a)
-
-y = a.symmetric_difference(c)
-print(y)
 
 
 
