d9cf1c765ef021dbe8d521d51face06e5d5d9235
Harnoor Kaur <harnoor6ninthbhvm@gmail.com>
2026-03-27 21:22:51 +0530

    Added Functions

---
+++ Functions.py
@@ -0,0 +1,52 @@
+# def hello():
+#     print("Hello, Harnoor!")
+
+# def add():
+#     x = 45
+#     y = 56
+#     print(x + y)
+
+# hello()
+# add()
+
+# def add(x, y): # Parameters
+#     print(x + y)
+
+# add(12, 34) # Arguments
+# add(89, 78)
+
+
+# Arbitary Arguments
+# def hello(*name):
+#     print("Hello", name[1])
+
+# hello("Harnoor", "Tanveer", "Aditi")
+
+# Return Statement
+# def hello():
+#     return "Hello World!"
+
+# print(hello())
+
+# def add(a, b):
+#     return a + b
+# print(add(3, 4))
+
+# Recursion
+# Infinite Loop
+# def hello():
+#     return hello()
+
+# print(hello())
+
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     else :
+#         return n * factorial(n-1)
+    
+# print(factorial(5))
+ 
+
+
+
