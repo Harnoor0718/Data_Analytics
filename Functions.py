96e2ba98adec6743a7d289eee9d5787e879aa204
Harnoor Kaur <harnoor6ninthbhvm@gmail.com>
2026-03-28 23:25:45 +0530

    Added Functions 2

--- Functions.py
+++ Functions.py
@@ -46,6 +46,14 @@
 #         return n * factorial(n-1)
     
 # print(factorial(5))
+
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     else :
+#         return n * factorial(n-1)
+    
+# print(factorial(5))
  
 
 
