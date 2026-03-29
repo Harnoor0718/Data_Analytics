e30841f24b2d4530b856c60a628ee43f796878a0
Harnoor Kaur <harnoor6ninthbhvm@gmail.com>
2026-03-29 21:28:01 +0530

    Added Modules

---
+++ modules.py
@@ -0,0 +1,32 @@
+# DateTime Module
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x)
+
+# y = datetime.datetime(2020, 5, 17)
+# print(y)
+# print(y.strftime("%A"))
+# print(y.strftime("%a"))
+# print(y.strftime("%B"))
+# print(y.strftime("%b"))
+# print(y.strftime("%Y"))
+
+# Random Module
+# import random
+# x = random.randint(1, 100)
+# print(x)
+
+# l = ["Harnoor", "Tanveer", "Aditi", "Sahil"]
+# print(random.choice(l))
+
+# Math Module
+
+import math 
+a = pow(2, 4)
+print(a)
+print(math.sqrt(16))
+print(math.pi)  
+print(math.fabs(-5))
+print(math.ceil(3.2))
+print(math.floor(3.8))
